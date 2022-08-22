ce7f010cceda9ce7a32d09749f937c68a8040d15
Charles Megafu <charles.megafu@bydeluxe.com>
2022-08-22 16:12:00 -0400

    ONECOND-2106: Add the ability for an HTTP task to provision a custom failure reason

--- contribs/src/main/java/com/netflix/conductor/contribs/http/Output.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/http/Output.java
@@ -1,14 +1,27 @@
 package com.netflix.conductor.contribs.http;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.ws.rs.core.MediaType;
-import java.util.HashMap;
-import java.util.Map;
+import static com.netflix.conductor.contribs.http.GenericHttpTask.CUSTOM_FAILURE_REASON_PARAMETER_NAME;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Output {
+
+	private ResponseFailureReason responseFailureReason;
+
 	private Validate validate;
+
+	@JsonProperty(CUSTOM_FAILURE_REASON_PARAMETER_NAME)
+	public ResponseFailureReason getResponseFailureReason() {
+		return responseFailureReason;
+	}
+
+	public void setResponseFailureReason(ResponseFailureReason responseFailureReason) {
+		this.responseFailureReason = responseFailureReason;
+	}
 
 	public Validate getValidate() {
 		return validate;
