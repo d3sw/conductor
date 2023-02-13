694896db452d647498efec2f0d0cb0a0e170fa26
pradeeppalat01 <pradeeppalat01@gmail.com>
2023-02-13 14:57:44 -0800

    Fixing script issues

--- postgresql-persistence/src/main/resources/db/migrations/V4_1__priority_config_table_insert_values.sql
+++ postgresql-persistence/src/main/resources/db/migrations/V4_1__priority_config_table_insert_values.sql
@@ -1,6 +1,10 @@
-INSERT INTO META_PRIORITY (CREATED_ON,MODIFIED_ON,MIN_PRIORITY,MAX_PRIORITY,name,VALUE) VALUES
-	 ('2023-01-30 11:22:23.990','2023-01-30 11:22:23.990',1,2,'vfs-server','urgent'),
-	 ('2023-01-30 11:22:23.990','2023-01-30 11:22:23.990',3,4,'vfs-server','high'),
-	 ('2023-01-30 11:22:23.990','2023-01-30 11:22:23.990',5,8,'vfs-server','normal'),
-	 ('2023-01-30 11:22:23.990','2023-01-30 11:22:23.990',9,10,'vfs-server','low'),
+CREATE UNIQUE INDEX IF NOT EXISTS meta_priority_uniq_idx ON META_PRIORITY(min_priority, max_priority, name);
+
+INSERT INTO META_PRIORITY (MIN_PRIORITY, MAX_PRIORITY, name, value) VALUES
+	 (1,2,'vfs-server','urgent'),
+	 (3,4,'vfs-server','high'),
+	 (5,8,'vfs-server','normal'),
+	 (9,10,'vfs-server','low')
 	on conflict do nothing;
+
+DROP TABLE IF EXISTS CONFIG_STORE;
