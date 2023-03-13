b55f511294c89bfd87781e270a969000cc553661
Charles Megafu <charles.megafu@bydeluxe.com>
2023-03-13 19:15:16 -0400

    ONECOND-2209: Add graceful shutdown to conductor

--- postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraBaseDAO.java
+++ postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraBaseDAO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.netflix.conductor.aurora.sql.*;
 import com.netflix.conductor.core.execution.ApplicationException;
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +27,8 @@ public abstract class AuroraBaseDAO {
 		"toString"
 	);
 
+	public final String DATASOURCE_SHUTDOWN_MSG = "Request cannot be fulfilled at this time, system shutdown in progress. Please retry request";
+
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
 	protected final DataSource dataSource;
 	private final ObjectMapper mapper;
@@ -43,6 +46,11 @@ public abstract class AuroraBaseDAO {
 	}
 
 	<R> R getWithTransaction(TransactionalFunction<R> function) {
+		HikariDataSource datasource = (HikariDataSource) dataSource;
+		if (datasource.isClosed()) {
+			throw new RuntimeException(DATASOURCE_SHUTDOWN_MSG);
+		}
+
 		Instant start = Instant.now();
 		LazyToString callingMethod = getCallingMethod();
 		if (logger.isTraceEnabled())
--- postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraMetadataDAO.java
+++ postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraMetadataDAO.java
@@ -27,6 +27,7 @@ public class AuroraMetadataDAO extends AuroraBaseDAO implements MetadataDAO {
 	private static final String PROP_TASKDEF_CACHE_REFRESH = "conductor.taskdef.cache.refresh.time.seconds";
 	private static final int DEFAULT_TASKDEF_CACHE_REFRESH_SECONDS = 60;
 	private final ConcurrentHashMap<String, TaskDef> taskDefCache = new ConcurrentHashMap<>();
+	private final ScheduledExecutorService executorService;
 
 	@Inject
 	public AuroraMetadataDAO(DataSource dataSource, ObjectMapper mapper, Configuration config) {
@@ -34,18 +35,8 @@ public class AuroraMetadataDAO extends AuroraBaseDAO implements MetadataDAO {
 		refreshTaskDefs();
 
 		int cacheRefreshTime = config.getIntProperty(PROP_TASKDEF_CACHE_REFRESH, DEFAULT_TASKDEF_CACHE_REFRESH_SECONDS);
-		ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		executorService = Executors.newSingleThreadScheduledExecutor();
 		executorService.scheduleWithFixedDelay(this::refreshTaskDefs, cacheRefreshTime, cacheRefreshTime, TimeUnit.SECONDS);
-
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			try {
-				logger.info("Closing refreshTaskDefs pool");
-				executorService.shutdown();
-				executorService.awaitTermination(5, TimeUnit.SECONDS);
-			} catch (Exception e) {
-				logger.debug("Closing refreshTaskDefs pool failed " + e.getMessage(), e);;
-			}
-		}));
 	}
 
 	@Override
@@ -63,6 +54,16 @@ public class AuroraMetadataDAO extends AuroraBaseDAO implements MetadataDAO {
 		validate(taskDef);
 		taskDef.setUpdateTime(System.currentTimeMillis());
 		return insertOrUpdateTaskDef(taskDef);
+	}
+
+	public void shutdown() {
+		try {
+			logger.info("Closing refreshTaskDefs pool");
+			executorService.shutdown();
+			executorService.awaitTermination(5, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			logger.error("failed to shutdown refreshTaskDefs pool");
+		}
 	}
 
 	@Override
