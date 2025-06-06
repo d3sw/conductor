02aab8146d0cd928d01d88f978b9611c79ac60fd
Milind Raut <milind.raut@bydeluxe.com>
2025-06-06 09:24:28 -0700

    Update V10_1__add_indexes.sql

--- postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
+++ postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
@@ -1,3 +1,17 @@
 CREATE INDEX IF NOT EXISTS queue_message_popped ON queue_message (popped);
 
 CREATE INDEX IF NOT EXISTS queue_message_version ON queue_message (version);
+
+CREATE INDEX IF NOT EXISTS queue_message_queue_name ON queue_message (queue_name);
+
+CREATE INDEX IF NOT EXISTS queue_message_message_id ON queue_message (message_id);
+
+CREATE INDEX IF NOT EXISTS workflow_error_registry_workflow_id ON workflow_error_registry (workflow_id);
+
+CREATE INDEX IF NOT EXISTS task_in_progress_task_id ON task_in_progress (task_id);
+
+CREATE INDEX IF NOT EXISTS event_execution_execution_id ON event_execution (execution_id);
+
+CREATE INDEX IF NOT EXISTS task_task_refname ON task (task_refname);
+
+CREATE INDEX IF NOT EXISTS task_workflow_id ON task (workflow_id);
