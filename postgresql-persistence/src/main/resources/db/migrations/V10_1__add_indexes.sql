ea206f646edc71c342949c4357350c574c8666a9
sangeetanadgir <sangeeta.nadgir@bydeluxe.com>
2025-06-06 20:18:44 +0530

    ONECOND-2363

--- postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
+++ postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
@@ -1,5 +1,3 @@
 CREATE INDEX IF NOT EXISTS queue_message_popped ON queue_message (popped);
 
 CREATE INDEX IF NOT EXISTS queue_message_version ON queue_message (version);
-
-CREATE INDEX IF NOT EXISTS queue_message_queue_name ON queue_message (queue_name);
