6b3d922520c1af991d50c4942e3ec3e67d228f23
pradeeppalat01 <pradeeppalat01@gmail.com>
2025-06-09 10:59:43 -0700

    refactoring

--- postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
+++ postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
@@ -6,6 +6,10 @@ CREATE INDEX IF NOT EXISTS queue_message_queue_name ON queue_message (queue_name
 
 CREATE INDEX IF NOT EXISTS queue_message_message_id ON queue_message (message_id);
 
+CREATE INDEX IF NOT EXISTS queue_message_queue_name_message_id ON queue_message (queue_name, message_id);
+
+CREATE INDEX IF NOT EXISTS queue_message_queue_name_deliver_on_popped ON queue_message (queue_name, deliver_on, popped);
+
 CREATE INDEX IF NOT EXISTS workflow_error_registry_workflow_id ON workflow_error_registry (workflow_id);
 
 CREATE INDEX IF NOT EXISTS task_in_progress_task_id ON task_in_progress (task_id);
@@ -16,13 +20,8 @@ CREATE INDEX IF NOT EXISTS task_task_refname ON task (task_refname);
 
 CREATE INDEX IF NOT EXISTS task_workflow_id ON task (workflow_id);
 
-
-CREATE INDEX IF NOT EXISTS queue_message_queue_name_deliver_on_popped ON queue_message (queue_name, deliver_on, popped);
-
-CREATE INDEX IF NOT EXISTS queue_message_queue_name_message_id ON queue_message (queue_name, message_id);
+CREATE INDEX IF NOT EXISTS task_task_id ON task (task_id);
 
 CREATE INDEX IF NOT EXISTS workflow_workflow_id ON workflow (workflow_id);
-
-CREATE INDEX IF NOT EXISTS task_task_id ON task (task_id);
 
 CREATE INDEX IF NOT EXISTS workflow_workflow_id_workflow_type ON workflow (workflow_id, workflow_type);
