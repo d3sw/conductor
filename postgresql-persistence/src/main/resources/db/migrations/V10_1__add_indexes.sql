a28b9d31c80602259ab13c5e0d6657875b3ad683
blam28 <46462301+blam28@users.noreply.github.com>
2025-06-09 09:20:43 -0700

    ONECOND-2363 add db indices
    add db indices

--- postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
+++ postgresql-persistence/src/main/resources/db/migrations/V10_1__add_indexes.sql
@@ -15,3 +15,14 @@ CREATE INDEX IF NOT EXISTS event_execution_execution_id ON event_execution (exec
 CREATE INDEX IF NOT EXISTS task_task_refname ON task (task_refname);
 
 CREATE INDEX IF NOT EXISTS task_workflow_id ON task (workflow_id);
+
+
+CREATE INDEX IF NOT EXISTS queue_message_queue_name_deliver_on_popped ON queue_message (queue_name, deliver_on, popped);
+
+CREATE INDEX IF NOT EXISTS queue_message_queue_name_message_id ON queue_message (queue_name, message_id);
+
+CREATE INDEX IF NOT EXISTS workflow_workflow_id ON workflow (workflow_id);
+
+CREATE INDEX IF NOT EXISTS task_task_id ON task (task_id);
+
+CREATE INDEX IF NOT EXISTS workflow_workflow_id_workflow_type ON workflow (workflow_id, workflow_type);
